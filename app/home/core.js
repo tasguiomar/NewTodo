9953a1033f62e8f5094f6c47306eb7c15541a03b
Unknown <tiagoalexandresanto@gmail.com>
2018-10-23 17:03:52 +0100

    login check : problem controller todo

---
+++ app/home/core.js
@@ -0,0 +1,58 @@
+var scotchTodo = angular.module('scotchTodo', []);
+angular
+        .module('scotchTodo')
+		.controller('mainController', mainController);
+		
+function mainController($scope, $http) {
+	$scope.formData = {};
+
+	$http.get('/api/todos')
+		.success(function(data) {
+			$scope.todos = data;
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+
+
+	$scope.createTodo = function() {
+		$http.post('/api/todos', $scope.formData)
+			.success(function(data) {
+				$scope.formData = {}; // clear the form so our user is ready to enter another
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+	$scope.filterFunction = function(element) {
+		return element.name.match(/^Ma/) ? true : false;
+	};
+
+
+	$scope.deleteTodo = function(id) {
+		$http.delete('/api/todos/' + id)
+			.success(function(data) {
+				$scope.todos = data;
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+	$scope.logOut = function(){
+		$location.path('/');
+	};
+
+	$scope.editTodo = function(){
+		var filterTodos = $scope.todos.filterTodos(function(todo){
+			if(todo.editTodo){
+				return todo;
+			};
+		});
+		dataService.createTodo(filterTodos);
+	};
+
+}
---
+++ server.js
@@ -0,0 +1,48 @@
+﻿require('rootpath')();
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
+
+
+var database = require('./config/database'); 			// load the database config
+var morgan = require('morgan'); 		// log requests to the console (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var mongoose = require('mongoose'); 					// mongoose for mongodb
+
+
+
+mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+app.use(morgan('dev')); 
+app.use(methodOverride());
+
+require('./app/routes.js')(app);
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
+
+// use JWT auth to secure the api
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
+
+// routes
+app.use('/login', require('./controllers/login.controller'));
+app.use('/register', require('./controllers/register.controller'));
+app.use('/app', require('./controllers/app.controller'));
+app.use('/api/users', require('./controllers/api/users.controller'));
+
+// make '/app' default route
+app.get('/', function (req, res) {
+    return res.redirect('/app');
+});
+
+// start server
+var server = app.listen(3000, function () {
+    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
+});
