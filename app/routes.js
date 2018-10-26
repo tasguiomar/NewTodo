14bc03b7e1ba43fb143a6da9f936ab61523088ee
Unknown <tiagoalexandresanto@gmail.com>
2018-10-26 16:40:53 +0100

    string secret melhorada

--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,19 @@
 var Todo = require('./models/todo');
-//const basicAuth = require('basic-auth');
 var username = require('username');
 var jwt = require('jsonwebtoken');
-var userService = require('../services/user.service');
+var userService = require('../config.json');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
 	// api ---------------------------------------------------------------------
 	// get all todos
-	app.get('/api/todos',verifyToken , function(req, res) {
-		
+	app.get('/api/todos', verifyToken, function (req, res) {
+
 		// use mongoose to get all todos in the database
 		Todo.find({
-			 userDb : req.result.sub}, function(err, todos) {
-			
+			userDb: req.result.sub
+		}, function (err, todos) {
+
 			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
 			if (err)
 				res.send(err)
@@ -23,20 +23,21 @@ module.exports = function(app) {
 	});
 
 	// create todo and send back all todos after creation
-	app.post('/api/todos',verifyToken, function(req, res) {
+	app.post('/api/todos', verifyToken, function (req, res) {
 		console.log("----------------estou aqui------------");
 
 		Todo.create({
-			userDb : req.result.sub,
-			text : req.body.text,
-			done : false
-		}, function(err, todo) {
+			userDb: req.result.sub,
+			text: req.body.text,
+			done: false
+		}, function (err, todo) {
 			if (err)
 				res.send(err);
 
 			// get and return all the todos after you create another
 			Todo.find({
-				userDb : req.result.sub},function(err, todos) {
+				userDb: req.result.sub
+			}, function (err, todos) {
 				if (err)
 					res.send(err)
 				res.json(todos);
@@ -46,27 +47,23 @@ module.exports = function(app) {
 	});
 
 	// delete a todo
-	app.delete('/api/todos/:todo_id',verifyToken  ,function(req, res) {
+	app.delete('/api/todos/:todo_id', verifyToken, function (req, res) {
 		Todo.remove({
-			_id : req.params.todo_id
-		}, function(err, todo) {
+			_id: req.params.todo_id
+		}, function (err, todo) {
 			if (err)
 				res.send(err);
 
 			// get and return all the todos after you create another
-			Todo.find({userDb : req.result.sub},function(err, todos) {
+			Todo.find({
+				userDb: req.result.sub
+			}, function (err, todos) {
 				if (err)
 					res.send(err)
 				res.json(todos);
 			});
 		});
 	});
-
-	// application -------------------------------------------------------------
-	/*app.get('*', function(req, res) {
-		res.sendfile('/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-	});*/
-
 
 	function verifyToken(req, res, next) {
 		//get auth header value
@@ -79,31 +76,23 @@ module.exports = function(app) {
 			//get token from array
 			const bearerToken = bearer[1];
 			//set the token
-	
+
 			req.token = bearerToken;
-
 		
+			jwt.verify(bearerToken, userService.secret, (err, result) => {
 
-			jwt.verify(bearerToken,"lS14MmXF3NL3Rchdm2abOCJYDbz81X9s2fVo1dsz8V8accZmTwSyTL0KZ0ujSQVszBy8t3BUzoafEnq6hCEPCZNRLt7yDtDtHtoS",(err, result)=>{
-			
-					if (err) {
-						res.sendStatus(403);
-					} else {
-						req.result = result;
-							next();
-					}
+				if (err) {
+					res.sendStatus(403);
+				} else {
+					req.result = result;
+					next();
+				}
 			})
-			
-			
 		} else {
 			res.sendStatus(403);
-			
-	
-		
+
 		}
 
-		
 	}
-	
-};
 
+};
